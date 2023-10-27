42da3e484d79726fd377a25b4001a0800c36c399
vinh <vinhkem1504@gmail.com>
2023-10-27 10:07:00 +0700

    cart done 27/10

--- routes/cart.route.ts
+++ routes/cart.route.ts
@@ -1,15 +1,16 @@
 import express from "express";
-import { addBookToCart, clearCart, getAllCart, getCartByUserId, deleteOneTypeBook, removeOneBook } from "../controllers/cart.controller";
+import { addBookToCart, clearCart, getAllCart, getCart, getCartByUserId, deleteOneTypeBook, removeOneBook } from "../controllers/cart.controller";
 import checkRole from "../middlewares/checkRole";
 import { verifyToken } from "../middlewares/verifyToken";
 
 const Router = express.Router()
 
 Router.route('/all').get(verifyToken,checkRole('admin'),getAllCart)
-Router.route('/:userId').get(verifyToken, checkRole("all"), getCartByUserId)
-.post(verifyToken, checkRole("all"), addBookToCart)
-.delete(verifyToken, checkRole("all"), deleteOneTypeBook)
-Router.route('/:userId/clear-cart').put(verifyToken, checkRole("all"), clearCart)
-Router.route('/:userId/remove').delete(verifyToken, removeOneBook)
+Router.route('/get-cart').get(verifyToken, checkRole("all"), getCart)
+Router.route('/add-book').post(verifyToken, checkRole("all"), addBookToCart)
+Router.route('/clear-cart').put(verifyToken, checkRole("all"), clearCart)
+Router.route('/delete-book').delete(verifyToken, checkRole("all"), deleteOneTypeBook)
+Router.route('/remove-book').delete(verifyToken, removeOneBook)
+Router.route('/get-cart/:userId').get(verifyToken, checkRole("admin"), getCartByUserId)
 
 export default Router
